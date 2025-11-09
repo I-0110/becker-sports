ca4dd9481f7d683ea63badaade40176f5d844f78
IvelisBecker <ivelisbecker@gmail.com>
2025-11-09 06:03:57 -0600

    Completed Ch.11 and fixed Search UI error

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,9 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
-  console.log(`Searching... ${term}`);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
  
   const params = new URLSearchParams(searchParams);
   params.set('page', '1');
@@ -39,4 +38,4 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}}
+}
