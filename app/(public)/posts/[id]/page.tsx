828fb457adfc9e36b13c5e306c5aff2fa1a70c6e
IvelisBecker <ivelisbecker@gmail.com>
2025-12-03 11:41:23 -0600

    Debug done

--- app/(public)/posts/[id]/page.tsx
+++ app/(public)/posts/[id]/page.tsx
@@ -7,8 +7,12 @@ import Link from 'next/link';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import Nav from '@/app/ui/nav';
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
+export default async function PostPage({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+    const { id } = await params;
     const post = await fetchPostById(id);
 
     if (!post) {
